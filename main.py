6bd247988f36fadb4dae71a7f48974a56eef1871
avangoor1 <ananya.vangoor@gmail.com>
2025-05-25 14:06:42 -0500

    time updated

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def blockTime(date, timevalue):
 
     input_login = driver.find_element(By.ID, "login")
 
-    input_login.send_keys("raghu2007")
+    input_login.send_keys("avangoor")
 
     input_password = driver.find_element(By.ID, "password")
 
@@ -104,4 +104,4 @@ def blockTime(date, timevalue):
 
 
 if __name__ == "__main__":
-    blockTime("05/30/2025", "5:00pm")
+    blockTime("06/02/2025", "5:30pm")
